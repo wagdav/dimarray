a9baf4bbbe6a12ad883fb0e68c494ec04e6d8b36
Kornel Jahn <kornel.jahn@gmail.com>
2011-01-19 12:33:00 +0100

    Removed the _context attribute from class DimArray entirely. Removed corresponding doctest lines, too, needs to be tidied up. All doctests passed.

--- dimarray/dimarray.py
+++ dimarray/dimarray.py
@@ -4,51 +4,35 @@ class DimArray(np.ndarray):
     """
     >>> dims = [('a', [0,1,2,3,4,5]), ('b', [0,1,2])]
     >>> a = DimArray(np.arange(6*3).reshape((6,3)), dims=dims)
-    >>> a._context == None
-    True
     >>> b = a[1:3,0::2]
-    >>> b._context == None
-    True
-
-    >>> a._context == None # Context after creating b must be None
-    True
-
     >>> a.dims == (('a', [0, 1, 2, 3, 4, 5]), ('b', [0, 1, 2]))
     True
     >>> b.dims == (('a', [1, 2]), ('b', [0, 2]))
     True
-
     >>> c = b[:,0]
     >>> c.dims  == (('a', [1, 2]), ('b', 0))
     True
     """
     def __new__(cls, input_array, dims=None):
         obj = np.asarray(input_array).view(cls)
-
         obj.dims = tuple(dims)
-        obj._context = None
-
         return obj
 
     def __array_finalize__(self, obj):
         if obj is None: return
 
-        self.dims = getattr(obj, 'dims', None)
-        context = getattr(obj, '_context', None)
-        if context:
-            self.dims = context
-            self._context = None
-            obj._context = None
+        if not hasattr(self, 'dims'):
+            self.dims = getattr(obj, 'dims', None)
 
     def __array_wrap__(self, out_arr, context=None):
         out_arr.dims = getattr(self, 'dims', None)
-        out_arr._context = None
         return np.ndarray.__array_wrap__(self, out_arr, context)
 
     def __getitem__(self, y):
-        self._context=_dims_getitem(self.dims, y)
-        return np.ndarray.__getitem__(self, y)
-
+        out_arr = np.ndarray.__getitem__(self, y)
+        if not np.isscalar(out_arr):
+            out_arr.dims = _dims_getitem(self.dims, y)
+        return out_arr
 
 def _dims_getitem(dimensions, key):
     """
@@ -82,4 +66,4 @@ def _dims_getitem(dimensions, key):
 
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=False)
