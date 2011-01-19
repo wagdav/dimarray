e0a64cb8d3b822d62600c8a165b2ba84e0badf53
David Wagner <david.wagner@epfl.ch>
2011-01-19 23:08:55 +0100

    Implement slices containing Ellipsis.

--- dimarray/dimarray.py
+++ dimarray/dimarray.py
@@ -1,4 +1,7 @@
 """
+1D case
+-------
+
 >>> a = DimArray(range(10),dims=[('t',range(10))])
 >>> a.dims == (('t', range(10)),)
 True
@@ -6,8 +9,35 @@ True
 >>> a[:5].dims == (('t', [0, 1, 2, 3, 4]),)
 True
 
+
+2D case
+-------
+
+>>> dims = [('a', [0,1,2,3,4,5]), ('b', [0,1,2])]
+>>> a = DimArray(np.arange(6*3).reshape((6,3)), dims=dims)
+>>> b = a[1:3,0::2]
+>>> a.dims == (('a', [0, 1, 2, 3, 4, 5]), ('b', [0, 1, 2]))
+True
+>>> b.dims == (('a', [1, 2]), ('b', [0, 2]))
+True
+>>> c = b[:,0]
+>>> c.dims  == (('a', [1, 2]), ('b', 0))
+True
+
+
+Slices with Ellipsis
+--------------------
+
+>>> dims = [('a', '12'), ('b', '123'), ('c', '1234'), ('d', '12345')]
+>>> a = DimArray(np.arange(2*3*4*5).reshape((2,3,4,5)), dims=dims)
+>>> a[0,...,0].shape
+(3, 4)
+
+>>> a[0,...,0].dims
+(('a', '1'), ('b', '123'), ('c', '1234'), ('d', '1'))
 """
 
+from itertools import izip_longest
 import numpy as np
 
 class DimArray(np.ndarray):
@@ -15,13 +45,6 @@ class DimArray(np.ndarray):
     >>> dims = [('a', [0,1,2,3,4,5]), ('b', [0,1,2])]
     >>> a = DimArray(np.arange(6*3).reshape((6,3)), dims=dims)
     >>> b = a[1:3,0::2]
-    >>> a.dims == (('a', [0, 1, 2, 3, 4, 5]), ('b', [0, 1, 2]))
-    True
-    >>> b.dims == (('a', [1, 2]), ('b', [0, 2]))
-    True
-    >>> c = b[:,0]
-    >>> c.dims  == (('a', [1, 2]), ('b', 0))
-    True
     """
     def __new__(cls, input_array, dims=None):
         obj = np.asarray(input_array).view(cls)
@@ -76,12 +99,27 @@ def _dims_getitem(dimensions, key):
     if isinstance(key, slice):
         key = (key,)
 
-    from itertools import izip_longest
-    ret=[]
-    for rng, dim in izip_longest(key, dimensions, fillvalue=slice(None)):
+    if key is Ellipsis:
+        key = (slice(None),)
+
+    key = list(key)
+    ret = []
+    ipad, i = 0, 0
+    for rng, dim in izip_longest(key, dimensions,
+                            fillvalue=slice(None)):
+        if rng is Ellipsis:
+            ipad = len(dimensions) - len(key)
+            rng = slice(None)
+
+        if ipad:
+            key.insert(i+1, slice(None))
+            ipad = ipad - 1
+
         dim_name, dim_range = dim
         new_range = dim_range[rng]
+
         ret.append((dim_name, new_range))
+        i = i + 1
 
     return tuple(ret)
 
